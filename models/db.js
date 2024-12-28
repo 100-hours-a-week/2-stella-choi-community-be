4ed0d10200c4169ef900cf121430cbd1e164e27b
choiyoorim <yulim3839@naver.com>
2024-12-28 14:39:44 +0900

    bug: 🐛 DB 연결시 시간대 자동 변경 해제

--- models/db.js
+++ models/db.js
@@ -9,6 +9,7 @@ const pool = mariadb.createPool({
     connectionLimit: 15, // 최대 연결 개수 설정
     idleTimeout: 10000, // 유휴 상태 유지 시간 (ms)
     acquireTimeout: 10000, // 연결 대기 시간 (ms)
+    dateStrings: true,
 });
 
 module.exports = pool;
