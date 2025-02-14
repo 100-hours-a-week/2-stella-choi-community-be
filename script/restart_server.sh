b64bf2b39349848b7810c60af390078b5d2c27ea
choiyoorim <yulim3839@naver.com>
2025-02-14 22:44:54 +0900

    feat: ✨ 서버 재시작 스크립트 추가

---
+++ script/restart_server.sh
@@ -0,0 +1,5 @@
+echo '======================'
+echo 'Running restart_server'
+echo '======================'
+
+pm2 restart /home/ubuntu/2-stella-choi-community-be/index.js --name community-be --watch
